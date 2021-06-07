3722a7e3d9b5c8e0cea17594bafba0a563bf37f1
nguyendat288 <you@example.com>
2021-06-07 11:48:40 +0700

    a

---
+++ src/AreaRect.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AreaRect {
+    public static void main (String[] args){
+        float a,b;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        System.out.println("Area = "+(a*b));
+    }
+}
